82356c62be3a43b6f31cd2f9fe5ff20af2728b34
EHEWON <EHEWON@users.noreply.github.com>
2024-09-10 07:49:29 +0000

    deploy: 259050b6517d5239e141ad30b8c46cb383aa54a9

---
+++ static/index-DlImoOiK.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-D8yVIb3r.js";function n(){return t({url:"setting/api",method:"get"})}function i(e){return t({url:"setting/api",method:"post",data:e})}function a(){return t({url:"setting/other",method:"get"})}function o(e){return t({url:"setting/other",method:"post",data:e})}export{a,o as b,n as g,i as s};

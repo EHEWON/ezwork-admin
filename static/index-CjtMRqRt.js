1574a00c9073c7407d22115d0413e935f2329693
EHEWON <EHEWON@users.noreply.github.com>
2024-09-10 08:06:32 +0000

    deploy: 2a39fceb7984b2e7c705f7b50bcc934accfb0d58

---
+++ static/index-CjtMRqRt.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-B6msb5_W.js";function n(){return t({url:"setting/api",method:"get"})}function i(e){return t({url:"setting/api",method:"post",data:e})}function a(){return t({url:"setting/other",method:"get"})}function o(e){return t({url:"setting/other",method:"post",data:e})}export{a,o as b,n as g,i as s};
